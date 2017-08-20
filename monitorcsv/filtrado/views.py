e913fed8a18799363baca92d207badd871dc31cd
Ramiro <ramirovazquez95@gmail.com>
2017-08-20 18:18:52 -0500

    filtos funcionando en conjunto

--- monitorcsv/filtrado/views.py
+++ monitorcsv/filtrado/views.py
@@ -28,8 +28,8 @@ def ver_registros(request):
         for row in archivo:
           #  print epoch_to_string(row[0])
             registros.append(row)
-    print "------------ registros"
-    print len(registros)
+    logger.debug("------------ registros")
+    logger.debug(len(registros))
     ##### todos
 
 
@@ -42,18 +42,27 @@ def ver_registros(request):
             registros_filtrados = []
 
 
-
-            for item in registros:
-                if item[1] == form.cleaned_data['status'] or item[2] == form.cleaned_data['url']:
-                    registros_filtrados.append(item)
+            if form.cleaned_data['status']:
+                registros_filtrados = [item for item in registros if item[1] == form.cleaned_data['status']]                   
           
-            '''
-            for item in registros_filtrados:
-                # se filtra la fecha de inicio
-                if form.cleaned_data['fecha_inicio']:
-                    if float(item[0]) >= float(form.cleaned_data['fecha_inicio']):
-                        registros_filtrados.append(item)
+            if form.cleaned_data['url']:
+                registros_filtrados = [item for item in registros_filtrados if item[2] == form.cleaned_data['url']]                   
 
+
+            logger.debug("------------ registros filtrados")
+            logger.debug(len(registros_filtrados))
+
+
+            if form.cleaned_data['fecha_inicio']:
+                logger.debug("ENTRA FECHA INICIO")
+
+                registros_filtrados = [item for item in registros_filtrados if form.cleaned_data['fecha_inicio'] if float(item[0]) >= float(form.cleaned_data['fecha_inicio'])]
+                logger.debug(len(registros_filtrados))
+
+            if form.cleaned_data['fecha_fin']:
+                registros_filtrados = [item for item in registros_filtrados if form.cleaned_data['fecha_fin'] if float(item[0]) <= float(form.cleaned_data['fecha_fin'])]
+
+            '''
             
             for item in registros_filtrados:
                 # se filtra la fecha de finn
